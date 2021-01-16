2af1584ba95bae918c110b170fcc097dd3748073
RodrigoGomesNascimento <rgomes.nascimento@gmail.com>
2021-01-16 19:54:20 -0300

    atualizar

---
+++ aula011/ex008.js
@@ -0,0 +1,7 @@
+//console.log('O console funcionou corretamento')
+var vel = 61
+console.log(`A velocidade do carro é ${vel}km/h`)
+if(vel > 60){//CONDIÇÃO SIMPLES.
+    console.log(`Voce ultrapassou o limite de velocidade. MULTADO!`)
+}
+console.log('Dirija sempre com cinto de seguranças')
